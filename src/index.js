7089f09b16575930884a65e1e684c797d99c5d22
LIWENQIANG <10382637@qq.com>
2018-07-26 18:42:41 +0800

    22

--- src/index.js
+++ src/index.js
@@ -2,19 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import store from './store.js';
-// import { addToCart, updateCart, deleteFromCart } from './actions/cart-actions';
 import { Provider } from 'react-redux';
-
-
-// store.dispatch(addToCart('Flour 1kg', 2, 110));
-// store.dispatch(addToCart('Juice 2L', 1, 250));
-
-// // Update Cart
-// store.dispatch(updateCart('Flour 1kg', 5, 110));
-
-// // Delete from Cart
-// store.dispatch(deleteFromCart('Coffee 500gm'));
-
 ReactDOM.render(
     <Provider store={store}>
         {<App />}
